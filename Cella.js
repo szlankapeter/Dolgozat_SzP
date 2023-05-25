ab0d5f4a8edfbfae245ec42f259f6621d973b80d
Szlanka Péter (SZF_2022_2024) <szlanka.peter@diak.szamalk-szalezi.hu>
2023-05-25 14:11:19 +0200

    jatekter

--- Cella.js
+++ Cella.js
@@ -5,15 +5,19 @@ class Cella{
     #divElem;
 
     constructor(id, adat, szuloElem){
-
+        this.id = id;
+        this.adat = adat;
+        let txt = `<div class="${id}">${adat}</div>`
+        szuloElem.append(txt);
+        
     }
 
     setAllapot(){
-
+        
     }
 
     szinBeallit(){
-
+        $("div:nth-child(odd)").css("background-color", "grey");
     }
 
     trigger(){
--- JatekTer.js
+++ JatekTer.js
@@ -1,12 +1,18 @@
+import Cella from "./Cella.js";
+
 class Jatekter{
 
     #db;
-    #allapotLista = [];
+    #allapotLista = ["@", "@", "@","", "", "","@", "@", "@"];
     #elemLista = [];
     #lepes;
 
     constructor(){
-
+        const ART = $("article");
+        for(let i=0;i<9;i++){
+            const cella = new Cella(i, "", ART);
+            cella.szinBeallit();
+        }
     }
 
     jatekter(){
